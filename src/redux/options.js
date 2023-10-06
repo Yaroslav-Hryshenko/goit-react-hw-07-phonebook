a043c0b640a57c4caa81c0a8bfecc37e30010430
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-10-06 13:46:47 +0300

    Update options.js

--- src/redux/options.js
+++ src/redux/options.js
@@ -3,40 +3,39 @@ import axios from 'axios';
 
 const BASIC_URL =
   'https://651946a6818c4e98ac6037f7.mockapi.io/Phonebook/contacts/';
-
 export const fetchContacts = createAsyncThunk(
-  'contatcts/fetchContacts',
+  'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get(BASIC_URL);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
-export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async (data, thunkAPI) => {
-    const { contactId } = data;
-    try {
-      await axios.delete(`${BASIC_URL}/${contactId}`);
-      return contactId;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      const { data } = await axios.get(BASIC_URL);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (contact, thunkAPI) => {
+  async (contacts, thunkAPI) => {
     try {
-      const res = await axios.post(BASIC_URL, contact);
-      return res.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      const { data } = await axios.post(BASIC_URL, contacts);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const { data } = await axios.delete(`${BASIC_URL}/${id}`);
+      return data; 
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
